aafa0b535609002210a1ea531b1e8c189bc77779
benutomo <52617232+benutomo-dev@users.noreply.github.com>
2021-12-10 20:14:23 +0900

    サンプルコード用プロジェクトを追加

---
+++ SampleCode/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+using Benutomo;
+
+namespace SampleCode
+{
+    // 自動実装を適用するクラス
+    [AutomaticDisposeImpl]
+    public partial class DisposeableTest : IDisposable, IAsyncDisposable
+    {
+        // DisposeableTestのDipose()とDiposeAsync()は自動実装されるため、定義不要
+
+        // IDisposable.Dispose()による破棄が可能なフィールド
+        [EnableAutomaticDispose]
+        ConsoleOutputDisposable consoleOutputDisposable = new ConsoleOutputDisposable();
+
+        // IDisposable.Dispose()とIAsyncDisposable.DisposeAsync()のどちらでも破棄が可能なプロパティ
+        [EnableAutomaticDispose]
+        ConsoleOutputAsyncDisposable consoleOutputAsyncDisposable { get; } = new ConsoleOutputAsyncDisposable();
+
+        public DisposeableTest()
+        {
+            Console.WriteLine("Created new DisposeableTest");
+        }
+    }
+
+    // 以降は、出力例のためのコード
+
+    class Program
+    {
+        public static async Task Main()
+        {
+            var disposeTestInstance = new DisposeableTest();
+
+            Console.WriteLine("Begin disposeTestInstance.Dispose()");
+            disposeTestInstance.Dispose();
+            Console.WriteLine("End disposeTestInstance.Dispose()");
+            Console.WriteLine();
+
+            var asyncDisposeTestInstance = new DisposeableTest();
+
+            Console.WriteLine("Begin disposeTestInstance.DisposeAsync()");
+            await asyncDisposeTestInstance.DisposeAsync();
+            Console.WriteLine("End disposeTestInstance.DisposeAsync()");
+            Console.WriteLine();
+        }
+    }
+
+    class ConsoleOutputDisposable : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("    Called Dispose() of ConsoleOutputDisposable.");
+        }
+    }
+
+    class ConsoleOutputAsyncDisposable : IDisposable, IAsyncDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("    Called Dispose() of ConsoleOutputAsyncDisposable.");
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            Console.WriteLine("    Called DisposeAsync() of ConsoleOutputAsyncDisposable.");
+            return default;
+        }
+    }
+}
---
+++ SampleCode/Sample1.cs
@@ -0,0 +1,33 @@
+﻿using Benutomo;
+using System;
+
+// 同期的な破棄(IDisposable)を自動実装
+[AutomaticDisposeImpl]
+partial class Sample1 : IDisposable
+{
+    // 自動破棄するメンバにはEnableAutomaticDispose属性を付与
+    [EnableAutomaticDispose]
+    IDisposable? _disposable;
+
+    // 自動破棄しないメンバにはDisableAutomaticDispose属性を付与
+    [DisableAutomaticDispose]
+    IDisposable Disposable => _disposable;
+}
+
+// 非同期的な破棄(IAsyncDisposable)を自動実装
+[AutomaticDisposeImpl]
+partial class Sample2 : IAsyncDisposable
+{
+}
+
+// 同期的な破棄(IDisposable)と非同期的な破棄(IAsyncDisposable)を自動実装
+[AutomaticDisposeImpl]
+partial class Sample3 : IDisposable, IAsyncDisposable
+{
+}
+
+// インターフェイスが明示的に実装されていないため、NG。IDisposableとIAsyncDisposableの少なくとどちらか一方の実装が必要。
+//[AutomaticDisposeImpl]
+//partial class Sample4
+//{
+//}
