fd874ecec5534cfa4609426d2e88a795f2b8a49c
benutomo <52617232+benutomo-dev@users.noreply.github.com>
2022-09-24 01:41:56 +0900

    テストの修正

--- Benutomo.AutomaticDisposeImpl.Analyzer.Test/AutomaticDisposeAnalyzerTests.cs
+++ Benutomo.AutomaticDisposeImpl.Analyzer.Test/AutomaticDisposeAnalyzerTests.cs
@@ -779,7 +779,7 @@ partial class A : IDisposable
             var expected2 = new[]
             {
                 Verify.Diagnostic("SG0010").WithLocation(10, 10),
-                DiagnosticResult.CompilerError("CS7036").WithLocation(genSourcePath, 44, 26), // 生成されるコードでシグネチャ違反によるCS7036も発生
+                DiagnosticResult.CompilerError("CS7036").WithLocation(genSourcePath, 45, 26), // 生成されるコードでシグネチャ違反によるCS7036も発生
             };
 
             await Verify.VerifyAnalyzerAsync(source2, expected2);
@@ -892,7 +892,7 @@ partial class A : IAsyncDisposable
             var expected1 = new[]
             {
                 Verify.Diagnostic("SG0014").WithLocation(10, 9),
-                DiagnosticResult.CompilerError("CS1061").WithLocation(genSourcePath, 56, 40), // 生成されるコードでConfigureAwait()未定義によるCS1061も発生
+                DiagnosticResult.CompilerError("CS1061").WithLocation(genSourcePath, 57, 40), // 生成されるコードでConfigureAwait()未定義によるCS1061も発生
             };
 
             await Verify.VerifyAnalyzerAsync(source, expected1);
@@ -914,7 +914,7 @@ partial class A : IAsyncDisposable
             var expected2 = new[]
             {
                 Verify.Diagnostic("SG0014").WithLocation(10, 10),
-                DiagnosticResult.CompilerError("CS7036").WithLocation(genSourcePath, 56, 28), // 生成されるコードでシグネチャ違反によるCS7036も発生
+                DiagnosticResult.CompilerError("CS7036").WithLocation(genSourcePath, 57, 28), // 生成されるコードでシグネチャ違反によるCS7036も発生
             };
 
             await Verify.VerifyAnalyzerAsync(source2, expected2);
@@ -935,7 +935,7 @@ partial class A : IAsyncDisposable
             var expected3 = new[]
             {
                 Verify.Diagnostic("SG0014").WithLocation(10, 15),
-                DiagnosticResult.CompilerError("CS7036").WithLocation(genSourcePath, 56, 28), // 生成されるコードでシグネチャ違反によるCS7036も発生
+                DiagnosticResult.CompilerError("CS7036").WithLocation(genSourcePath, 57, 28), // 生成されるコードでシグネチャ違反によるCS7036も発生
             };
 
             await Verify.VerifyAnalyzerAsync(source3, expected3);
